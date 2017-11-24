e6973c9cb9fac96a0223ffd51d7ddd4d33bf375d
CristianBanan <cristian.ursache96@yahoo.com>
2017-11-24 12:31:11 +0200

    done project!

---
+++ Animals/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Animal
+    {
+        public string name { get; set; }
+
+        public string sound { get; set; }
+
+        public Animal(string Name, string Sound)
+        {
+            name = Name;
+            sound = Sound;
+        }
+
+        public override string ToString()
+        {
+            return name + " makes " + sound; 
+        }
+
+    }
+}
--- Animals/Program.cs
+++ Animals/Program.cs
@@ -10,6 +10,31 @@ namespace Animals
     {
         static void Main(string[] args)
         {
+            Animal dog = new Animal("Dog", "Woof-woof");
+
+            Animal cat = new Animal("Cat", "Meow!");
+
+            Animal cow = new Animal("Cow", "Moo!");
+
+            Animal frog = new Animal("Frog", "Craok!");
+
+            Animal fish = new Animal("Fish", "Blub!");
+
+            List<Animal> list = new List<Animal>();
+            list.Add(dog);
+            list.Add(cat);
+            list.Add(cow);
+            list.Add(frog);
+            list.Add(fish);
+
+            foreach(Animal a in list)
+            {
+                Console.WriteLine(a.ToString()); 
+            }
+
+            Console.ReadLine();
+
+
         }
     }
 }
